3b301db64a030bb7f6bccd7dd4b75e143cbcc5c7
syed zohaib <zohaibsyed914@gmail.com>
2025-12-09 16:21:54 +0500

    working

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ const isProduction = window.location.hostname !== "localhost";
 
 const axiosInstance = axios.create({
     baseURL: isProduction
-        ? ""
+        ? "https://task-manager-server-two-zeta.vercel.app"
         : "http://localhost:3000",
 });
 
